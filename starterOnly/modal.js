a9c4550d24cefbe8122f9ce4538383f4ba385bca
Florent Eeckeman <florenteeckeman@live.fr>
2023-05-16 09:09:16 +0200

    add comment

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,14 +15,12 @@ const btnClose = document.getElementById("btn-close");
 const closeBtn = document.querySelector(".close");
 const formBtn = document.getElementById("btn-submit");
 const checkboxError = document.getElementById("email");
-var form = document.getElementById("myForm");
+
+// listener for form submit
 formBtn.addEventListener("click", function (event) {
   event.preventDefault();
   getValue();
 });
-
-const regex =
-  /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.addEventListener("click", closeModal);
@@ -37,18 +35,12 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// form management
-//firstName manager
-
+// function for collect form datas
 function getValue() {
-  //var reportVal = form.reportValidity();
   const checkTest = handleData();
-  console.log("checkTest : " + checkTest);
   if (checkTest === true) {
     var formData = new FormData(document.querySelector("form"));
-    for (var key of formData.keys()) {
-      console.log(key);
-    }
+
     for (var value of formData.values()) {
       console.log(value);
     }
@@ -58,29 +50,25 @@ function getValue() {
     document.getElementById("btn-close").style.display = "block";
   }
 }
-function ValidationInputText(alert, input) {
-  const border = input.style.border;
-  alert.style.display = "block";
+//function for bad entry on text input change style
+function ValidationInputText(error, input) {
+  error.style.display = "block";
   input.style.border = "2px solid #FF4E60 ";
   setTimeout(() => {
     input.style.border = "0.8px solid #ccc";
-    alert.style.display = "none";
+    error.style.display = "none";
   }, 3000);
 }
-function ValidationInputButton(alert, input) {
-  const border = input.style.border;
-  alert.style.display = "block";
+//function for bad entry on button input change style
+function ValidationInputButton(error, input) {
+  error.style.display = "block";
   input.style.border = "2px solid #FF4E60 ";
   setTimeout(() => {
     input.style.border = "none";
-    alert.style.display = "none";
+    error.style.display = "none";
   }, 3000);
 }
-
-function getVal() {
-  const val = document.getElementById("first").value;
-  console.log(val);
-}
+//function for check form validity
 function handleData() {
   var form_data = new FormData(document.querySelector("form"));
   const firstValue = document.getElementById("first");
@@ -91,10 +79,11 @@ function handleData() {
   const locateValue = document.getElementById("locate-box");
   const checkyValue = document.getElementById("CGU-Accept");
 
+  //select error span elements
   var firstErrors = document.querySelector("#first-validity");
   var lastErrors = document.querySelector("#last-validity");
   var emailErrors = document.querySelector("#email-validity");
-  var birthdateErrors = document.querySelector("#birthdate-validity");
+  var birthDateErrors = document.querySelector("#birthdate-validity");
   var quantityErrors = document.querySelector("#quantity-validity");
   var checkboxErrors = document.querySelector("#locate-validity");
   var cguErrors = document.querySelector("#cgu-validity");
@@ -122,7 +111,7 @@ function handleData() {
   }
 
   if (!birthValue.checkValidity()) {
-    ValidationInputText(birthdateErrors, birthValue);
+    ValidationInputText(birthDateErrors, birthValue);
     validation = false;
   }
   if (!form_data.has("location")) {
@@ -134,8 +123,4 @@ function handleData() {
     validation = false;
   }
   return validation;
-}
-
-function Validate() {
-  console.log("trigger");
 }
